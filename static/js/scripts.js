3cc9f7e962aa7058adb483792bd8086275ce11bb
subham <shubham.gaurav.1994@gmail.com>
2016-05-10 19:33:19 +0530

    added ajax

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,21 +1,119 @@
 $(document).ready(function(){
-	// set the height of cards to equal lengths
-	function equalizeCardHeights() {
-		var maxHeight    = 0;
-	    var cardContents = $(".card .card-content");
-	    cardContents.each(function(){
-	        if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
-	    });
-	    cardContents.height(maxHeight);
+
+    // set the height of cards to equal lengths
+    function equalizeCardHeights() {
+    	var maxHeight    = 0;
+        var cardContents = $(".card .card-content");
+        cardContents.each(function(){
+            if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
+        });
+        cardContents.height(maxHeight);
+    }
+
+    equalizeCardHeights();
+
+    // function to get a csrfTokenVal to be passed into the headers
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+    // opens the modal pop up window
+    function openModal(modalSelector) {
+        $(modalSelector).openModal({
+		    dismissible: false, // Modal can be dismissed by clicking outside of the modal
+		    opacity: .7, // Opacity of modal background
+		    in_duration: 400, // Transition in duration
+		    out_duration: 300, // Transition out duration
+    	});
+    }
+
+    function makeAjaxRequest(url, type, data, dataType, async, csrftoken, success_callback) {
+		$.ajax({
+	        url: url,
+	        type: type,
+	        data: data || null,
+	        dataType: dataType,
+	        async: async,
+	        success: function (response) {
+	            success_callback(response);
+	        },
+         	headers: {
+                'X-CSRFToken': csrftoken
+            }
+		});
+    }
+
+    function doCommonPostWork(response, selector) {
+    	var msg = JSON.parse(response);
+    	if (msg.error) {
+    		var errorArea = $(selector);
+    		errorArea.html('')
+    		$.each(msg.error, function(errName, description) {
+    			errorArea.append(
+    				'<p style="color: #b71c1c" class="news-title">\
+    				 <i class="ion-close-circled"></i> ' + errName + ': ' + description[0] + '</p>'
+    			);
+    		});
+    	} else {
+    		window.location.href = msg.redirect_to;      
+    	}
+    }
+		      
+	// load add to-do form to modal dynamically
+	$("a.fixed-element").click(function (event) {
+		event.preventDefault();
+		openModal('#modal1');
+    	makeAjaxRequest('/add_todo/', 'get', {}, 'html', true, null, function(response) {
+    		$('.modal-content').html(response);
+    		post_todo_form();
+    	});
+    });
+
+	// post the form data
+	function post_todo_form() {
+		$("#add_todo_form").submit(function (event) {
+			event.preventDefault();
+			var data = { title: $("#id_title").val(), desc: $("#id_desc").val() }
+			makeAjaxRequest('/add_todo/', 'post', data, null, true, csrftoken, function(response) {
+				doCommonPostWork(response, '.error-area');
+			});
+		});
 	}
 
-	equalizeCardHeights();
+	// load edit to-do form to modal dynamically
+	$(".edit-todo").click(function (event) {
+		event.preventDefault();
+		var todo_id = $(this).data("id");
+        openModal('#modal1');
+        makeAjaxRequest('/edit_todo/', 'get', {id: todo_id}, 'html', true, null, function(response) {
+        	$('.modal-content').html(response);
+        	edit_todo_form(todo_id);
+        });
+    });
 
-	// sidebar
-	$('.button-collapse').sideNav({
-	    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-	  }
-	);
-	      
+    // post the form data
+    function edit_todo_form(todo_id) {
+    	$("#edit_todo_form").submit(function (event) {
+    		event.preventDefault();
+    		var data = { id: todo_id, title: $("#id_title").val(), desc: $("#id_desc").val() }
+    		makeAjaxRequest('/edit_todo/', 'post', data, null, true, csrftoken, function(response) {
+    			doCommonPostWork(response, '.error-area');
+    		});
+    	});
+    }
+
 
 });
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
+import json
 from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from todo.models import ToDo
@@ -14,56 +15,81 @@ def delete_db_entry(id):
 		# todo doesn't exist with the given id, do nothing.
 		pass
 
+
 def get_profile_data(todos):
 	pending_todos = todos.exclude(is_deleted=True).order_by('-created_at')
 	deleted_todos = todos.filter(is_deleted=True).order_by('-updated_at')
 	return (pending_todos, deleted_todos)
 
+
 def home(request):
 	todos = ToDo.objects.all().order_by('-created_at').exclude(is_deleted=True)
 	return render_to_response('todos.html', {'todos': todos}, context_instance=RequestContext(request))
+
 
 def delete_todo(request, todo_id):
 	delete_db_entry(todo_id)
 	return HttpResponseRedirect('/')
 
+
 def delete_todo_profile(request, todo_id):
 	delete_db_entry(todo_id)
 	return HttpResponseRedirect('/profile')
 
+
 @login_required
 def add_todo(request):
-	if request.method == "POST":
+	if request.is_ajax() and request.method == "POST":
 		form = ToDoForm(request.POST)
 		if form.is_valid():
-			todo = form.save(commit=False)
-			todo.creator = request.user
-			todo.save()
-			return HttpResponseRedirect('/')
+			title = request.POST.get('title')
+			desc = request.POST.get('desc')
+			ToDo.objects.create(
+				title=title, desc=desc, creator=request.user
+			)
+			return HttpResponse(json.dumps({"success": "Form submitted succesfully !", "redirect_to": '/'}))
+		else:
+			return HttpResponse(json.dumps({"error": form.errors}))
 	else:
 		form = ToDoForm()
-	return render_to_response('add_todo.html', {'form': form}, context_instance=RequestContext(request))
+	return render_to_response('add_todo_form.html', {'form': form}, context_instance=RequestContext(request))
+
 
 def edit_todo_helper(request_obj, todo_id, redirect_to=''):
-	todo = ToDo.objects.filter(pk=todo_id)
-	if request_obj.method == "POST":
+	todo = ToDo.objects.filter(pk=todo_id).exclude(is_deleted=True)
+	if request_obj.is_ajax() and request_obj.method == "POST":
 		form = ToDoForm(request_obj.POST)
 		if form.is_valid():
-			title = request_obj.POST['title']
-			desc = request_obj.POST['desc']
+			title = request_obj.POST.get('title')
+			desc = request_obj.POST.get('desc')
 			todo.update(title=title, desc=desc)
-			return HttpResponseRedirect('/' + redirect_to)
+			return HttpResponse(json.dumps({"success": "Form submitted succesfully !", "redirect_to": '/' + redirect_to}))
+		else:
+			return HttpResponse(json.dumps({"error": form.errors}))
 	else:
 		form = ToDoForm(initial={'title': todo[0].title, 'desc': todo[0].desc})
-	return render_to_response('add_todo.html', {'form': form, 'action': 'edit_todo/' + str(todo_id)}, 
-							   context_instance=RequestContext(request_obj))
+	return render_to_response('add_todo_form.html', {'form': form, 'action': 'edit_todo/' + str(todo_id)}, 
+									   context_instance=RequestContext(request_obj))
 
 
-def edit_todo(request, todo_id):
+@login_required
+def edit_todo(request):
+	if request.method == "GET":
+		todo_id = request.GET.get('id')
+	else:
+		todo_id = request.POST.get('id')
+		print(todo_id)
 	return edit_todo_helper(request, todo_id)
 
+
+@login_required
 def edit_todo_profile(request, todo_id):
+	if request.method == "GET":
+		todo_id = request.GET.get('id')
+	else:
+		todo_id = request.POST.get('id')
 	return edit_todo_helper(request, todo_id, 'profile')
+
 
 @login_required
 def profile(request):
@@ -71,6 +97,7 @@ def profile(request):
 	pending_todos, deleted_todos = get_profile_data(todos)
 	return render_to_response('profile.html', {'todos': pending_todos, 'deleted_todos': deleted_todos}, 
 							   context_instance=RequestContext(request))
+
 
 def delete_trash(request, todo_id):
 	try:
@@ -80,6 +107,7 @@ def delete_trash(request, todo_id):
 		# todo doesn't exist with the given id, do nothing.
 		pass
 	return HttpResponseRedirect('/profile')
+
 
 def get_user(request, user_id):
 	try:
